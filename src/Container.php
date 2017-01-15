ac8e3be880c02341e15a039cddd7db3d60df06e2
Home <home@MacBook-Air-de-Irfan.local>
2017-01-16 00:07:33 +0100

    Container class added

---
+++ src/Container.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace IrfanTOOR;
+
+use Pimple\Container as PimpleContainer;
+
+class Container extends PimpleContainer
+{
+	function __construct($init=[]) {
+		parent::__construct($init);	
+	}
+
+	# you can set a key using this notation: $c->hello = 'world'
+	function __set($key, $value) {
+		$this->set($key, $value);
+	}
+	
+	# you can get a key using this notation: $h = $c->hello
+	# @return Mixed - the value of the key is returned
+	function __get($key) {
+		$this->get($key);
+	}
+	
+	# checks if a key exists $c->has('hello')
+	# @return Boolean
+	public function has($key) {
+		return $this->offsetExists($key);
+	}
+	
+	# you can get the value of key, or the default value if does not exist
+	public function get($key, $default = null)
+	{
+		if ($this->has($key))
+			return $this->offsetGet($key);
+		
+		return $default;
+	}
+	
+	# you can set the value of key to value or an array of assignments
+	public function set($key, $value=null) {	
+		if (is_array($key)) {
+			foreach($key as $k => $v) {
+				$this->set($k, $v);
+			}
+		} else {
+			$this->offsetSet($key, $value);
+		}
+	}
+	
+	# clear a key
+	public function clear($key) {
+		$this->offsetUnset($key);
+	}
+}
