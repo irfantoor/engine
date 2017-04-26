214470fa97b51c02b9894c18f3a6675130c9213a
Irfan TOOR <email@irfantoor.com>
2017-04-26 13:16:42 +0200

    Dependency of container-interop removed

--- src/Container.php
+++ src/Container.php
@@ -2,55 +2,53 @@
 
 namespace IrfanTOOR\Engine;
 
-use Interop\Container\ContainerInterface;
-use IrfanTOOR\Engine\Exception\ContainerException;
-use IrfanTOOR\Engine\Exception\IdNotStringException;
-use IrfanTOOR\Engine\Exception\NotFoundException;
-
-class Container implements ContainerInterface
+class Container
 {
-	protected $raw;
-
-	/**
-	 * Constructs a container and initializes with the initial data
-	 * 
-	 * @param array $init contains a key, value array, so that the container be initialized
-	 */
-	function __construct($init=[]) {
-        $this->factories = [];
-		$this->raw = [];
-
-		# Initialize Container
-		$this->set($init);
-	}
-
+    protected $data=[];
+    
     /**
-     * Finds an entry of the container by its identifier and returns it.
+     * Constructs the container with an intial data array
      *
-     * @param string $id Identifier of the entry to look for.
-     * @param mixed $args used as arguments to be passed to a closure/factory function
+     */
+    public function __construct($init = null) 
+    {
+    	if (is_array($init))
+	    	$this->data = $init;
+    }
+    
+    /**
+     * Sets an entity in the container
      *
-     * @throws IdNotStringException  Identity id, was not a string.
-     * @throws NotFoundException  No entry was found for this identifier: id.
-     * @throws ContainerException Error while retrieving the entry referred by identifier: id.
+     * @param $id		string	Identifier
+     * @param $value	mixed	Value
+     */
+	public function set($id, $value)
+	{
+		$this->data[$id] = $value;
+	}
+	
+	/**
+	 * Removes the entity from the container 
+	 *
+	 * @param $id	String		Identifier
+	 */
+	public function remove($id) {
+		if ($this->has($id))
+			unset($this->data[$id]);
+	}
+    
+    /**
+     * Finds an entry of the container by its identifier and returns it or returns the default
+     *
+     * @param string $id        Identifier of the entry to look for.
+     * @param mixed  $default   Default value in case the No Entry is found for the identifier
+     *                          in the container
      *
      * @return mixed Entry.
      */
-    public function get($id, $args=null) {
-    	if (!is_string($id))
-    		throw new IdNotStringException("Identity {$id}, was not a string");
-
-    	if (!$this->has($id))
-    		throw new NotFoundException("No entry was found for this identifier: {$id}");
-
-    	try {
-            if (array_key_exists($id, $this->factories))
-                return $this->raw[$id]($args);
-            else
-                return $this->raw[$id];
-    	} catch(\Exception $e) {
-			throw new ContainerException("Error while retrieving the entry referred by identifier: {$id}");
-    	}
+    public function get($id, $default=null)
+    {
+    	return $this->has($id) ? $this->data[$id] : $default;
     }
 
     /**
@@ -59,57 +57,19 @@ class Container implements ContainerInterface
      *
      * @param string $id Identifier of the entry to look for.
      *
-     * @return boolean
+     * @return bool
      */
-    public function has($id) {
-    	if (!is_string($id))
-    		return false;
-
-    	if (array_key_exists($id, $this->raw))
-    		return true;
-
-    	return false;
+    public function has($id){
+    	return array_key_exists($id, $this->data);
     }
-
+    
     /**
-     * Sets a entry for a given identifier: id
+     * Returns the container as array
      *
-     * @param string $id Identifier of the entry or an array of key, value pairs
-     * @param string $id Identifier of the entry.
-     *
-     * @throws IdNotStringException  Identity id, was not a string.
+     * returns Array
      */
-    public function set($id=null, $entry=null) {
-        if (is_array($id)) {
-            foreach ($id as $k => $v) {
-                $this->set($k, $v);
-            }
-        }
-        else {
-            if (!is_string($id))
-                throw new IdNotStringException("Identity {$id}, was not a string");
-
-            if (!is_string($entry) && is_callable($entry)) {
-                $this->factories[$id] = true;
-            }
-
-            $this->raw[$id] = $entry;
-        }
-    }
-
-    /**
-     * Removes an entry for the given identifier.
-     *
-     * @param string $id Identifier of the entry to look for.
-     */
-    public function remove($id) {
-        if (!is_string($id))
-            throw new IdNotStringException("Identity {$id}, was not a string");
-
-        if ($this->has($id)) {
-            $this->set($id, null);
-            unset($this->factories[$id]);
-            unset($this->raw[$id]);
-        }
+    public function raw() 
+    {
+    	return $this->data;
     }
 }
