775a09410748f35bb997cac6371c35ac986326ac
Irfan TOOR <email@irfantoor.com>
2017-05-10 01:50:04 +0200

    $ie, $config and function show added to controller

--- src/Controller.php
+++ src/Controller.php
@@ -3,9 +3,16 @@
 namespace IrfanTOOR\Engine;
 
 class Controller
-{	
-	function __construct() {}
-		
+{
+	protected 
+		$ie,
+		$config;
+	
+	function __construct() {
+		$this->ie = IE::getInstance();
+		$this->config = $this->ie->config->toArray();
+	}
+	
 	function __call($func, array $args)
 	{
 		return $this->default_method($args);
@@ -13,6 +20,11 @@ class Controller
 	
 	function default_method() 
 	{
-		throw new Exception('default_method must be defined in your controller');
+		throw new Exception("default_method must be defined in your controller");
+	}
+	
+	function show($view, $data=[]) {
+		$v = new View($view);
+		$v->show($data);
 	}
 }
